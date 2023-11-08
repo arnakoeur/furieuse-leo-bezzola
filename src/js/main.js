531c12d6aa5bd37c6d4e5ac010c0261f834a1686
Leo Bezzola <98465742+arnakoeur@users.noreply.github.com>
2023-11-08 16:04:23 +0100

    fini cartes animé

--- src/js/main.js
+++ src/js/main.js
@@ -75,24 +75,200 @@ gsap.to(".para-title-fore", {
   y: -900,
 });
 
-gsap.from(".para-ysa-bg", {
-  scrollTrigger: {
-    trigger: ".para-ysa-bg",
-    start: "bottom bottom",
-    end: "top top",
-    markers: true,
-    scrub: true,
-  },
-  y: -50,
-});
-
 gsap.to(".para-ysa-bg", {
   scrollTrigger: {
     trigger: ".para-ysa-bg",
-    start: "bottom bottom",
-    end: "top top",
+    start: "top bottom",
+    end: "bottom top",
     markers: true,
     scrub: true,
   },
-  y: 90,
+  y: -100,
+});
+
+gsap.to(".para-ysa-chara", {
+  scrollTrigger: {
+    trigger: ".para-ysa-chara",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true,
+    scrub: true,
+  },
+  y: 150,
+});
+
+gsap.from(".para-ysa-chara", {
+  scrollTrigger: {
+    trigger: ".para-ysa-chara",
+    start: "bottom top",
+    end: "bottom top",
+    markers: true,
+    scrub: true,
+  },
+  y: 20,
+});
+
+gsap.from(".para-ysa-comic", {
+  scrollTrigger: {
+    trigger: ".para-ysa-comic",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true,
+    scrub: true,
+  },
+  x: -80,
+});
+
+gsap.to(".para-ysa-comic", {
+  scrollTrigger: {
+    trigger: ".para-ysa-comic",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true,
+    scrub: true,
+  },
+  x: 0,
+});
+
+gsap.to(".para-ysa-txt", {
+  scrollTrigger: {
+    trigger: ".para-ysa-txt",
+    start: "top bottom",
+    end: "bottom 80%",
+    markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+
+gsap.to(".para-roi-bg", {
+  scrollTrigger: {
+    trigger: ".para-roi-bg",
+    start: "50% bottom",
+    end: "bottom bottom",
+    markers: true,
+    scrub: true,
+  },
+  y: -30,
+});
+
+gsap.to(".para-roi-chara", {
+  scrollTrigger: {
+    trigger: ".para-roi-chara",
+    start: "50% bottom",
+    end: "bottom top",
+    markers: true,
+    scrub: true,
+  },
+  y: 100,
+});
+
+gsap.from(".para-roi-chara", {
+  scrollTrigger: {
+    trigger: ".para-roi-chara",
+    start: "bottom top",
+    end: "bottom top",
+    markers: true,
+    scrub: true,
+  },
+  y: 20,
+});
+
+gsap.from(".para-roi-comic", {
+  scrollTrigger: {
+    trigger: ".para-roi-comic",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true,
+    scrub: true,
+  },
+  x: 80,
+});
+
+gsap.to(".para-roi-comic", {
+  scrollTrigger: {
+    trigger: ".para-roi-comic",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true,
+    scrub: true,
+  },
+  x: 0,
+});
+
+gsap.to(".para-roi-txt", {
+  scrollTrigger: {
+    trigger: ".para-roi-txt",
+    start: "top bottom",
+    end: "bottom 80%",
+    markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
+});
+
+gsap.to(".para-epee-bg", {
+  scrollTrigger: {
+    trigger: ".para-epee-bg",
+    start: "top bottom",
+    end: "bottom bottom",
+    markers: true,
+    scrub: true,
+  },
+  y: -100,
+});
+
+gsap.to(".para-epee-chara", {
+  scrollTrigger: {
+    trigger: ".para-epee-chara",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true,
+    scrub: true,
+  },
+  y: 150,
+});
+
+gsap.from(".para-epee-chara", {
+  scrollTrigger: {
+    trigger: ".para-epee-chara",
+    start: "bottom top",
+    end: "bottom top",
+    markers: true,
+    scrub: true,
+  },
+  y: 20,
+});
+
+gsap.from(".para-epee-comic", {
+  scrollTrigger: {
+    trigger: ".para-epee-comic",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true,
+    scrub: true,
+  },
+  x: 80,
+});
+
+gsap.to(".para-epee-comic", {
+  scrollTrigger: {
+    trigger: ".para-epee-comic",
+    start: "top bottom",
+    end: "bottom top",
+    markers: true,
+    scrub: true,
+  },
+  x: 0,
+});
+
+gsap.to(".para-epee-txt", {
+  scrollTrigger: {
+    trigger: ".para-epee-txt",
+    start: "top bottom",
+    end: "bottom 80%",
+    markers: true,
+    scrub: true,
+  },
+  scale: 1.1,
 });
