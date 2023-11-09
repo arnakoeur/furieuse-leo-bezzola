8a0be6f225c66a441299f5b7f566a80bc1ee64a6
Leo Bezzola <98465742+arnakoeur@users.noreply.github.com>
2023-11-09 10:13:22 +0100

    debut du scroll horizontal

--- src/js/main.js
+++ src/js/main.js
@@ -272,3 +272,16 @@ gsap.to(".para-epee-txt", {
   },
   scale: 1.1,
 });
+
+gsap.to(".slider-track", {
+  scrollTrigger: {
+    trigger: ".slider-mask",
+    start: "top top",
+    end: "+=300%",
+    pin: true,
+    scrub: true,
+    markers: true,
+  },
+  x: "-75%",
+  ease: "sine.inOut",
+});
